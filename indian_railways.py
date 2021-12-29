384146e812f1d6639a6f907141c68e5b5b9ddefd
hDmtP <pritamdhara914@gmail.com>
2021-12-29 21:18:56 +0530

    less code(used generators), more optimized

--- indian_railways.py
+++ indian_railways.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-import requests
-from bs4 import BeautifulSoup
-import fontstyle
+def main():
+    import requests
+    from bs4 import BeautifulSoup
+    import fontstyle
+    n = int(input(fontstyle.apply("\nEnter any integer (Max Limit: 6 digits): ", "bold/Italic/green")))
+    url = f"https://indiarailinfo.com/blog/{n}?tp=1"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    content = soup.prettify()
+    print(fontstyle.apply("\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++", "bold/red/YELLOW_BG"))
+    print(fontstyle.apply(f"| {soup.title.string} |", "bold/white/BLACK_BG"))
+    print(fontstyle.apply("+++++++++++++++++++++++++++++++++++++++++++++++++++\n\n", "bold/red/YELLOW_BG"))
+    mygenerator = (i for i in soup.find_all('a', attrs = {"style":"font-weight:bold;"}))
+    for i in mygenerator:
+        if str(i.text) != "General Travel" and str(i.text) != "Advanced Search":
+            print(fontstyle.apply(f"{i.text}\n", "bold/Italic/cyan"))
 
-n = int(input(fontstyle.apply("\nEnter any integer (Max Limit: 6 digits): ", "bold/Italic/green")))
-url = f"https://indiarailinfo.com/blog/{n}?tp=1"
-
-r = requests.get(url)
-
-soup = BeautifulSoup(r.content, 'html5lib')
-
-content = soup.prettify()
-
-print(fontstyle.apply("\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++", "bold/red/YELLOW_BG"))
-print(fontstyle.apply(f"| {soup.title.string} |", "bold/white/BLACK_BG"))
-print(fontstyle.apply("+++++++++++++++++++++++++++++++++++++++++++++++++++\n\n", "bold/red/YELLOW_BG"))
-
-
-count = 0
-l=[]
-for i in soup.find_all('a', attrs = {"style":"font-weight:bold;"}):
-    if str(i.text) != "General Travel":
-        count += 1
-        l.append(i.text)
-
-for i in range(1, count):
-    print(fontstyle.apply(f"{l[i]}\n", "bold/Italic/cyan"))
-
-print(fontstyle.apply(f"\t\toutput count: {count}", "italic/purple"))
+if __name__ == "__main__":
+    main()
