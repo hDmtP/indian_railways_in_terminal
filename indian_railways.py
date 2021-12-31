cd5c347c9a6efb00bab7d0f3c17d788ddccd5423
hDmtP <pritamdhara914@gmail.com>
2021-12-31 22:35:58 +0530

    +RailFan quotes

--- indian_railways.py
+++ indian_railways.py
@@ -13,8 +13,13 @@ def main():
     print(fontstyle.apply("+++++++++++++++++++++++++++++++++++++++++++++++++++\n\n", "bold/red/YELLOW_BG"))
     mygenerator = (i for i in soup.find_all('a', attrs = {"style":"font-weight:bold;"}))
     for i in mygenerator:
-        if str(i.text) != "General Travel" and str(i.text) != "Advanced Search":
+        if str(i.text) != "General Travel" and str(i.text) != "Advanced Search" and str(i.text) != "x-under SW-x":
             print(fontstyle.apply(f"{i.text}\n", "bold/Italic/cyan"))
+            
+    mygen2 = (i for i in soup.find_all('h1', attrs = {"class":"pinkColor"}))
+    for i in mygen2:
+    	print(fontstyle.apply("\nQuote from a RF:", "bold/red"))
+    	print(fontstyle.apply(f"\n\t\t{i.text}", "italic/yellow"))
 
 if __name__ == "__main__":
     main()
